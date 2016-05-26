f046480d865a0c957ad8684b5b3799d218a5fd13
Gareth Lawson <garethpeter@gmail.com>
2016-05-26 08:24:46 +0200

    Add an example config for Mailgun to the env example file and add a default from address to the mail config

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@ruggedy.io', 'name' => 'Ruggedy [No Reply]'],
 
     /*
     |--------------------------------------------------------------------------
