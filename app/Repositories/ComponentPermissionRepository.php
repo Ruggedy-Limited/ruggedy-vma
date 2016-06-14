51e2f226c491e96fcc1ed281546ef533609d4787
Gareth Lawson <garethpeter@gmail.com>
2016-06-14 17:12:59 +0200

    Create repositories for components and component_permissions tables

--- app/Repositories/ComponentPermissionRepository.php
+++ app/Repositories/ComponentPermissionRepository.php
@@ -3,8 +3,58 @@
 namespace App\Repositories;
 
 use Doctrine\ORM\EntityRepository;
+use Illuminate\Support\Collection;
 
 
 class ComponentPermissionRepository extends EntityRepository
 {
+    /**
+     * Get all the permissions for a specific component instance
+     *
+     * @param int $componentInstanceId
+     * @return Collection
+     */
+    public function findByComponentInstanceId(int $componentInstanceId): Collection
+    {
+        if (!isset($componentInstanceId)) {
+            return new Collection();
+        }
+
+        $resultSet = $this->findBy([
+            'instance_id' => $componentInstanceId
+        ]);
+
+        if (empty($resultSet)) {
+            return new Collection();
+        }
+
+        return new Collection($resultSet);
+    }
+
+    /**
+     * Find a permission a permission entry by component_id, instance_id and user_id
+     *
+     * @param int $componentId
+     * @param int $componentInstanceId
+     * @param int $userId
+     * @return Collection
+     */
+    public function findByComponentInstanceAndUserIds(int $componentId, int $componentInstanceId, int $userId): Collection
+    {
+        if (!isset($componentId, $componentInstanceId, $userId)) {
+            return new Collection();
+        }
+
+        $resultSet = $this->findBy([
+            'component_id' => $componentId,
+            'instance_id'  => $componentInstanceId,
+            'user_id'      => $userId,
+        ]);
+
+        if (empty($resultSet)) {
+            return new Collection();
+        }
+
+        return new Collection($resultSet);
+    }
 }
--- app/Repositories/ComponentRepository.php
+++ app/Repositories/ComponentRepository.php
@@ -2,9 +2,24 @@
 
 namespace App\Repositories;
 
+use App\Entities\Component;
 use Doctrine\ORM\EntityRepository;
 
 
 class ComponentRepository extends EntityRepository
 {
+    /**
+     * Get the record by the Component name
+     *
+     * @param string $componentName
+     * @return array|null
+     */
+    public function findByComponentName(string $componentName)
+    {
+        if (empty($componentName)) {
+            return null;
+        }
+        
+        return $this->findBy([Component::COMPONENT_NAME_FIELD => $componentName]);
+    }
 }
