83a839269aa7c8c21d1d888ac07dbf6254d6ca43
Gareth Lawson <garethpeter@gmail.com>
2016-06-29 15:37:28 +0200

    Add regex constants for validation of certain Asset fields and add a wrapper for getTeam() to the User entity

--- app/Entities/Asset.php
+++ app/Entities/Asset.php
@@ -16,6 +16,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Asset extends Base\Asset implements SystemComponent
 {
+    /** Regular expressions used for validating the relevant Asset data fields */
+    const REGEX_CPE         = 'cpe:(?\d)(?\.\d):[aho](?::(?:[a-zA-Z0-9!"#$%&\'()*+,\\\-_.\/;<=>?@\[\]^`{|}~]|\\:)+){10}$';
+    const REGEX_MAC_ADDRESS = '^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$';
+    
     /**
      * Get the parent Entity of this Entity
      *
