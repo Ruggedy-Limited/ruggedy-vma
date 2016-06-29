7d950504ee092f76c649267af134260143a307ea
Gareth Lawson <garethpeter@gmail.com>
2016-06-29 16:02:19 +0200

    Create an Eloquent model for Assets for the Behat tests

---
+++ app/Asset.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Spark\Spark;
+
+class Asset extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'assets';
+}
