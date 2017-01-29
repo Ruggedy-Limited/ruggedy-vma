70e22380cb71bc4b4d1c12dcb8d4751a0fd569cf
Francois Marais <francois@marais.co.nz>
2017-01-30 10:28:08 +1300

    Basic theme files.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,76 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+    <div class="col-md-4">
+    <button class="flat-btn" type="button">Primary Button</button>
+    </div>
+    <div class="col-md-4">
+        <div class="card hovercard animated pulse-hover">
+            <div class="cardheader c-white">
+            </div>
+            <div class="avatar avatar-white">
+                <i class="fa fa-th-large fa-5x t-c-red"></i>
+            </div>
+            <div class="info">
+                <div class="title h-3">
+                    <h4>Workspace Card</h4>
+                </div>
+                <div class="desc t-3">Pellentesque lacinia sagittis libero. Praesent vitae justo purus. In hendrerit
+                    lorem nisl,
+                    ac lacinia urna aliquet non. Quisque nisi tellus, rhoncus quis est s, rhoncus quis est s,
+                    rhoncus quis est s, rhoncus quis est s, rhoncus quis est s, rhoncus quis est
+                </div>
+                <div class="adm-card-footer">
+                    <div class="pull-right">
+                        <i class="fa fa-pencil"></i>
+                        <i class="fa fa-trash p-l-8"></i>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    <div class="col-md-4">
+        <div class="card hovercard animated pulse-hover">
+            <div class="cardheader c-white">
+            </div>
+            <div class="avatar avatar-white">
+                <i class="fa fa-folder fa-5x t-c-red"></i>
+            </div>
+            <div class="info">
+                <div class="title h-3">
+                    <h4>Folder Card</h4>
+                </div>
+                <div class="desc t-3">Pellentesque lacinia sagittis libero. Praesent vitae justo purus. In hendrerit
+                    lorem nisl,
+                    ac lacinia urna aliquet non. Quisque nisi tellus, rhoncus quis est s, rhoncus quis est s,
+                    rhoncus quis est s, rhoncus quis est s, rhoncus quis est s, rhoncus quis est
+                </div>
+                <div class="adm-card-footer">
+                    <div class="pull-right">
+                        <i class="fa fa-pencil"></i>
+                        <i class="fa fa-trash p-l-8"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card hovercard animated pulse-hover">
+            <div class="cardheader c-white">
+            </div>
+            <div class="avatar avatar-white">
+                <img src="/img/nessus-logo.png">
+            </div>
+            <div class="info">
+                <div class="title h-3">
+                    <h4>Nessus Scanner</h4>
+                </div>
+                <div class="desc t-3">Pellentesque lacinia sagittis libero. Praesent vitae justo purus. In hendrerit
+                    lorem nisl,
+                    ac lacinia urna aliquet non. Quisque nisi tellus, rhoncus quis est s, rhoncus quis est s,
+                    rhoncus quis est s, rhoncus quis est s, rhoncus quis est s, rhoncus quis est
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ruggedy-App</title>
+    <link rel="stylesheet" href="{{ asset('/vendor/bootstrap/dist/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('/vendor/font-awesome/css/font-awesome.css') }}">
+    <link rel="stylesheet" href="{{ asset("/css/animate.min.css") }}">
+    <link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
+</head>
+
+<body>
+<div>
+    <nav class="navbar navbar-default navigation-clean-search">
+        <div class="container-nav">
+            <div class="navbar-header"><a class="navbar-brand navbar-link">Ruggedy.io </a>
+                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span
+                            class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span
+                            class="icon-bar"></span><span class="icon-bar"></span></button>
+            </div>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav">
+                    <li role="presentation"><a href="#"><span class="t-c-red">.W</span>orkspaces</a></li>
+                    <li role="presentation"><a href="#"><span class="t-c-red">.S</span>ettings</a></li>
+                    <li role="presentation"><a href="#"><span class="t-c-red">.P</span>rofile</a></li>
+                </ul>
+                <form class="navbar-form navbar-left" target="_self">
+                    <div class="form-group">
+                        <label class="control-label" for="search-field"><i
+                                    class="glyphicon glyphicon-search t-c-red"></i></label>
+                        <input class="form-control search-field" type="search" name="search" id="search-field">
+                    </div>
+                </form>
+                <a class="btn btn-default navbar-btn navbar-right action-button" role="button" href="">Logout </a>
+            </div>
+        </div>
+    </nav>
+</div>
+    <div class="container">
+
+        @yield('content')
+
+        <script src="{{ asset('/vendor/jquery/dist/jquery.js') }}"></script>
+        <script src="{{ asset('/vendor/bootstrap/dist/js/bootstrap.js') }}"></script>
+    </div>
+</body>
+
+</html>
