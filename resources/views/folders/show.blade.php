f9aabe32461971a1ec7d0075782974ca194b5afb
Francois Marais <francois@marais.co.nz>
2017-02-05 18:22:32 +1300

    Basic comments in folder.

--- resources/views/folders/show.blade.php
+++ resources/views/folders/show.blade.php
@@ -48,12 +48,17 @@
                             <span></span>
                             <h4 class="h-4-1">CVE-2014-6278 bash: code execution via specially crafted environment
                                 variables</h4>
-                            <p>GNU Bash through 4.3 bash43-026 does not properly parse function definitions in the values of
-                                environment variables, which allows remote attackers to execute arbitrary commands via a crafted
-                                environment, as demonstrated by vectors involving the ForceCommand feature in OpenSSH sshd, the
-                                mod_cgi and mod_cgid modules in the Apache HTTP Server, scripts executed by unspecified DHCP
+                            <p>GNU Bash through 4.3 bash43-026 does not properly parse function definitions in the
+                                values of
+                                environment variables, which allows remote attackers to execute arbitrary commands via a
+                                crafted
+                                environment, as demonstrated by vectors involving the ForceCommand feature in OpenSSH
+                                sshd, the
+                                mod_cgi and mod_cgid modules in the Apache HTTP Server, scripts executed by unspecified
+                                DHCP
                                 clients, and other situations in which setting the environment occurs across a privilege
-                                boundary from Bash execution. NOTE: this vulnerability exists because of an incomplete fix for
+                                boundary from Bash execution. NOTE: this vulnerability exists because of an incomplete
+                                fix for
                                 CVE-2014-6271, CVE-2014-7169, and CVE-2014-6277.</p>
                             <p><i class="fa fa-thumb-tack"></i> Workspace/App/File/Asset</p>
                         </div>
@@ -66,7 +71,8 @@
                 <div id=tab-content2 class=tab-content>
                     <div class="col-md-12">
                         <div class="list-content-card">
-                            <p>Use your operating system's package manager to upgrade GNU bash to the latest version.</p>
+                            <p>Use your operating system's package manager to upgrade GNU bash to the latest
+                                version.</p>
                         </div>
                     </div>
                 </div>
@@ -75,7 +81,109 @@
                 <input type=radio name=tabs id=tab3>
                 <label for=tab3>Comments</label>
                 <div id=tab-content3 class=tab-content>
-                    <div class="col-md-4">
+                    <div class="col-md-12">
+                        <div class="chat-card">
+                            <div class="panel-body body-panel">
+                                <ul class="chat">
+                                    <li>
+                                        <div class="chat-body">
+                                            <div class="header">
+                                                <strong class="primary-font">User Name</strong>
+                                                <p class="text-muted">
+                                                    <small class=" text-muted"><span class="fa fa-clock-o"></span>13
+                                                        mins
+                                                        ago
+                                                    </small>
+                                                </p>
+                                            </div>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                                bibendum ornare
+                                                dolor, quis ullamcorper ligula sodales. aasdfs fdsfsd fs dfdsfds hgjgjgjjghjgjgj
+                                            </p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="chat-body">
+                                            <div class="header">
+                                                <strong class="primary-font">User Name</strong>
+                                                <p class="text-muted">
+                                                    <small class=" text-muted"><span class="fa fa-clock-o"></span>13
+                                                        mins
+                                                        ago
+                                                    </small>
+                                                </p>
+                                            </div>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                                bibendum ornare
+                                                dolor, quis ullamcorper ligula sodales. aasdfs fdsfsd fs dfdsfds hgjgjgjjghjgjgj
+                                            </p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="chat-body">
+                                            <div class="header">
+                                                <strong class="primary-font">User Name</strong>
+                                                <p class="text-muted">
+                                                    <small class=" text-muted"><span class="fa fa-clock-o"></span>13
+                                                        mins
+                                                        ago
+                                                    </small>
+                                                </p>
+                                            </div>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                                bibendum ornare
+                                                dolor, quis ullamcorper ligula sodales. aasdfs fdsfsd fs dfdsfds hgjgjgjjghjgjgj
+                                            </p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="chat-body">
+                                            <div class="header">
+                                                <strong class="primary-font">User Name</strong>
+                                                <p class="text-muted">
+                                                    <small class=" text-muted"><span class="fa fa-clock-o"></span>13
+                                                        mins
+                                                        ago
+                                                    </small>
+                                                </p>
+                                            </div>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                                bibendum ornare
+                                                dolor, quis ullamcorper ligula sodales. aasdfs fdsfsd fs dfdsfds hgjgjgjjghjgjgj
+                                            </p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="chat-body">
+                                            <div class="header">
+                                                <strong class="primary-font">User Name</strong>
+                                                <p class="text-muted">
+                                                    <small class=" text-muted"><span class="fa fa-clock-o"></span>13
+                                                        mins
+                                                        ago
+                                                    </small>
+                                                </p>
+                                            </div>
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+                                                bibendum ornare
+                                                dolor, quis ullamcorper ligula sodales. aasdfs fdsfsd fs dfdsfds hgjgjgjjghjgjgj
+                                            </p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="chat-footer">
+                                <textarea class="black-form-control" rows="1"></textarea>
+                                <span class="pull-left">
+                                <button class="primary-btn" id="btn-chat">Post</button>
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </li>
