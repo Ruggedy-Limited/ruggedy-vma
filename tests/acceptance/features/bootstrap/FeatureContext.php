1cbb3ac7a201ea971c103a1fddcc331ed9a33878
Gareth Lawson <garethpeter@gmail.com>
2017-02-06 20:34:03 +0200

    Remove unnecessary debugging statements

--- tests/acceptance/features/bootstrap/FeatureContext.php
+++ tests/acceptance/features/bootstrap/FeatureContext.php
@@ -150,8 +150,6 @@ class FeatureContext extends MinkContext implements Context
         } catch (QueryException $e) {
             // Just catch the exception in the case of integrity constraint violations
             // Re-enable foreign key checks
-            $e->getMessage();
-            $e->getCode();
         }
 
         Schema::enableForeignKeyConstraints();
