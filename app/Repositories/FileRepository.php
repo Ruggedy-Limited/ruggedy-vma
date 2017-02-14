041a46176b747d1ab7d20ab6cee909fc6db3163f
Gareth Lawson <garethpeter@gmail.com>
2017-02-14 20:23:36 +0200

    When looking for unprocessed files fetch the parent WorkspaceApp and the related Workspace and ScannerApp all at once. We'll need these deeper in the code from when files were directly related to Workspace and ScannerApp

--- app/Repositories/FileRepository.php
+++ app/Repositories/FileRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Entities\File;
+use App\Entities\Workspace;
+use App\Entities\WorkspaceApp;
 use Doctrine\ORM\EntityRepository;
 use Illuminate\Support\Collection;
 
@@ -15,13 +17,19 @@ class FileRepository extends EntityRepository
      */
     public function findUnprocessed(): Collection
     {
-        $result = $this->findBy([
-            File::DELETED   => false,
-            File::PROCESSED => false,
-        ], [
-            File::WORKSPACE_ID => 'ASC',
-        ]);
+        $queryBuilder = $this->_em
+            ->createQueryBuilder()
+            ->select('f', 'wa', 'w', 's')
+            ->from(File::class, 'f')
+            ->leftJoin('f.' . File::WORKSPACEAPP, 'wa')
+            ->leftJoin('wa.' . WorkspaceApp::WORKSPACE, 'w')
+            ->leftJoin('wa.' . WorkspaceApp::SCANNERAPP, 's')
+            ->where('f.' . File::DELETED . ' = :deleted AND f.' . File::PROCESSED . ' = :processed')
+            ->setParameter('deleted', false)
+            ->setParameter('processed', false)
+            ->orderBy('w.' . Workspace::ID, 'ASC');
 
+        $result = $queryBuilder->getQuery()->getResult();
         if (empty($result)) {
             return new Collection();
         }
