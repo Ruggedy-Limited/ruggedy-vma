1d8dfa3e6652ebad08bdbf3f4b0b7679425f6baf
Francois Marais <francois@marais.co.nz>
2017-02-20 20:52:16 +1300

    Theme update.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-6 col-md-offset-3">
             <div class="content-card">
                 <br><br>
                 <div>
