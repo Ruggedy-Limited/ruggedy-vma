ed688c2e0a111e2b101d81d2dcbe72b15bd39cf8
Gareth Lawson <garethpeter@gmail.com>
2017-03-21 00:11:36 +0200

    More Workspace App and File functionality

---
+++ resources/views/workspaces/edit-file.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.main')
+
+@section ('breadcrumb')
+    <p>Breadcrumbs / Goes / Here
+        <button type="button" class="btn round-btn pull-right c-grey" data-toggle="modal" data-target="#help">
+            <i class="fa fa-question fa-lg" aria-hidden="true"></i>
+        </button>
+        <button type="button" class="btn round-btn pull-right c-yellow">
+            <i class="fa fa-times fa-lg" aria-hidden="true"></i>
+        </button>
+    </p>
+@endsection
+
+@section('content')
+
+    @include('layouts.formError')
+
+    <!-- Modal -->
+    <div id="help" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Help Ttile</h4>
+                </div>
+                <div class="modal-body">
+                    <p>Help text goes here.</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-4 col-sm-4 animated fadeIn">
+            <h3>Edit File: {{ $file->getName() }}</h3>
+            <br>
+            {!! Form::open(['url' => route('workspace.app.file.update', ['fileId' => $file->getId()])]) !!}
+            <div class="form-group">
+                {!! Form::label('name', 'Name') !!}
+                {!! Form::text('name', $file->getName(), ['class' => 'black-form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('description', 'Description') !!}
+                {!! Form::textarea('description', $file->getDescription(), ['class' => 'black-form-control', 'rows' => '3']) !!}
+            </div>
+            <button class="primary-btn" type="submit">Save Changes</button>
+            {!! Form::close() !!}
+        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-6 animated fadeInUp">
+
+        </div>
+    </div>
+
+@endsection
