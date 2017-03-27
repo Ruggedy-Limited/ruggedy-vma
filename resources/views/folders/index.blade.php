54f49646d72830869d2b57cac404953e37fd4904
Gareth Lawson <garethpeter@gmail.com>
2017-03-27 11:11:39 +0200

    Fix typos, comments etc.

--- resources/views/folders/index.blade.php
+++ resources/views/folders/index.blade.php
@@ -26,7 +26,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Help Ttile</h4>
+                    <h4 class="modal-title">Help Title</h4>
                 </div>
                 <div class="modal-body">
                     <p>Help text goes here.</p>
--- resources/views/workspaces/appShowRecord.blade.php
+++ resources/views/workspaces/appShowRecord.blade.php
@@ -51,7 +51,7 @@
                     </div>
                         <div class="form-group">
                             {!! Form::label('name', 'Password') !!}
-                            {!! Form::password('name', null, ['class' => 'black-form-control']) !!}
+                            {!! Form::password('name', ['class' => 'black-form-control']) !!}
                         </div>
                     </div>
                     <div class="col-md-6">
