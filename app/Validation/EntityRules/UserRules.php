84b5141fba19f11dbafec444940b8908208898d1
Gareth Lawson <garethpeter@gmail.com>
2017-04-12 21:45:37 +0200

    Remove the uniqueness rule here as it prevented saving changes when the email address was not changed.

--- app/Validation/EntityRules/UserRules.php
+++ app/Validation/EntityRules/UserRules.php
@@ -17,7 +17,7 @@ class UserRules extends AbstractEntityRules
     {
         return [
             'name'     => 'bail|required|min:2',
-            'email'    => 'bail|required|email|unique:'. User::class .',email',
+            'email'    => 'bail|required|email',
             'password' => 'present',
         ];
     }
