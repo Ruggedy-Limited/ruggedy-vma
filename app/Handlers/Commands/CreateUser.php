55269ad6ba021aee53df33e97caf642ef68725fc
Gareth Lawson <garethpeter@gmail.com>
2017-04-12 21:44:27 +0200

    Add checks for permission on User related operations (Settings).

--- app/Handlers/Commands/CreateUser.php
+++ app/Handlers/Commands/CreateUser.php
@@ -4,6 +4,7 @@ namespace App\Handlers\Commands;
 
 use App\Commands\CreateUser as CreateUserCommand;
 use App\Entities\User;
+use App\Exceptions\ActionNotPermittedException;
 use App\Exceptions\InvalidInputException;
 use App\Repositories\UserRepository;
 use Doctrine\ORM\EntityManager;
@@ -33,14 +34,22 @@ class CreateUser extends CommandHandler
      *
      * @param CreateUserCommand $command
      * @return User
+     * @throws ActionNotPermittedException
      * @throws InvalidInputException
      */
     public function handle(CreateUserCommand $command)
     {
+        $requestingUser = $this->authenticate();
+
         /** @var User $user */
         $user = $command->getEntity();
         if (empty($user) || empty($user->getName()) || empty($user->getEmail())) {
             throw new InvalidInputException("One or more required fields are empty");
+        }
+
+        // Make sure the authenticated User is an admin. Only admins can create User accounts
+        if (!$requestingUser->isAdmin()) {
+            throw new ActionNotPermittedException("Only admins can create Users.");
         }
 
         $this->em->persist($user);
