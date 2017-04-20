9ae34a839657e6c8043098541e75f5a07a0ad7e5
Gareth Lawson <garethpeter@gmail.com>
2017-04-20 18:15:16 +0200

    Add an asset- prefix to the names of the form fields to avoid conflicts with the vulnerability form.

--- resources/views/partials/asset-form.blade.php
+++ resources/views/partials/asset-form.blade.php
@@ -19,40 +19,40 @@
                     'name' => 'new-asset',
                 ]) !!}
                 <div class="form-group">
-                    {!! Form::label('name', 'Name') !!}
-                    {!! Form::text('name', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-name', 'Name') !!}
+                    {!! Form::text('asset-name', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('cpe', 'CPE') !!}
-                    {!! Form::text('cpe', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-cpe', 'CPE') !!}
+                    {!! Form::text('asset-cpe', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('vendor', 'Vendor') !!}
-                    {!! Form::select('vendor', $vendors, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-vendor', 'Vendor') !!}
+                    {!! Form::select('asset-vendor', $vendors, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('ip_address_v4', 'IP Address v4') !!}
-                    {!! Form::text('ip_address_v4', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-ip_address_v4', 'IP Address v4') !!}
+                    {!! Form::text('asset-ip_address_v4', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('ip_address_v6', 'IP Address v6') !!}
-                    {!! Form::text('ip_address_v6', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-ip_address_v6', 'IP Address v6') !!}
+                    {!! Form::text('asset-ip_address_v6', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('hostname', 'Hostname') !!}
-                    {!! Form::url('hostname', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-hostname', 'Hostname') !!}
+                    {!! Form::url('asset-hostname', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('mac_address', 'MAC Address') !!}
-                    {!! Form::text('mac_address', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-mac_address', 'MAC Address') !!}
+                    {!! Form::text('asset-mac_address', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('os_version', 'OS Version') !!}
-                    {!! Form::text('os_version', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-os_version', 'OS Version') !!}
+                    {!! Form::text('asset-os_version', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
-                    {!! Form::label('netbios', 'Netbios Name') !!}
-                    {!! Form::text('netbios', null, ['class' => 'black-form-control']) !!}
+                    {!! Form::label('asset-netbios', 'Netbios Name') !!}
+                    {!! Form::text('asset-netbios', null, ['class' => 'black-form-control']) !!}
                 </div>
                 <div class="form-group">
                     {!! Form::submit('Create Asset', ['class' => 'primary-btn']) !!}
