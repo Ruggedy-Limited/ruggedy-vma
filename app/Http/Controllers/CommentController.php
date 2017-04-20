b196f48c9111a8323359e140e6acb6eaf8006a31
Gareth Lawson <garethpeter@gmail.com>
2017-04-20 15:39:10 +0200

    Create new controllers with all the actions moved out of the WorkspaceController.

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Commands\CreateComment;
+use App\Commands\DeleteComment;
+use App\Commands\EditComment;
+use App\Commands\GetNewComments;
+use App\Entities\Comment;
+use Exception;
+
+class CommentController extends AbstractController
+{
+    /**
+     * Create a new comment
+     *
+     * @POST("/comment/create/{vulnerabilityId}", as="comment.create", where={"vulnerabilityId":"[0-9]+"})
+     *
+     * @param $vulnerabilityId
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function createComment($vulnerabilityId)
+    {
+        // Validate the request
+        $this->validate($this->request, $this->getValidationRules(), $this->getValidationMessages());
+
+        $comment = new Comment();
+        $comment->setContent($this->request->get('comment'));
+
+        $command = new CreateComment(intval($vulnerabilityId), $comment);
+        $comment = $this->sendCommandToBusHelper($command);
+
+        if ($this->isCommandError($comment)) {
+            return redirect()->back()->withInput();
+        }
+
+        $this->flashMessenger->success("A new comment was posted successfully.");
+        return redirect()->route('vulnerability.view', ['vulnerabilityId' => $vulnerabilityId]);
+    }
+
+    /**
+     * Edit an existing comment
+     *
+     * @POST("/comment/edit/{commentId}", as="comment.edit", where={"commentId":"[0-9]+"})
+     *
+     * @param $commentId
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function editComment($commentId)
+    {
+        // Validate the request
+        $this->validate($this->request, $this->getValidationRules(), $this->getValidationMessages());
+
+        $command = new EditComment(intval($commentId), [
+            Comment::CONTENT => $this->request->get('comment-' . $commentId)
+        ]);
+
+        $comment = $this->sendCommandToBusHelper($command);
+
+        if ($this->isCommandError($comment)) {
+            return redirect()->back()->withInput();
+        }
+
+        $this->flashMessenger->success("Comment updated successfully.");
+        return redirect()->back();
+    }
+
+    /**
+     * Delete an existing comment
+     *
+     * @GET("/comment/delete/{commentId}", as="comment.remove", where={"commentId":"[0-9]+"})
+     *
+     * @param $commentId
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function deleteComment($commentId)
+    {
+        $command = new DeleteComment($commentId, true);
+        $comment = $this->sendCommandToBusHelper($command);
+
+        if ($this->isCommandError($comment)) {
+            return redirect()->back();
+        }
+
+        $this->flashMessenger->success("Comment deleted successfully.");
+        return redirect()->back();
+    }
+
+    /**
+     * Get new comments
+     *
+     * @POST("/comments/updated/{vulnerabilityId}", as="comments.get.updated", where={"vulnerabilityId":"[0-9]+"})
+     *
+     * @param $vulnerabilityId
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     * @throws Exception
+     */
+    public function getNewComments($vulnerabilityId)
+    {
+        $command = new GetNewComments(
+            $vulnerabilityId,
+            $this->request->get('newer-than', '0000-00-00 00:00:00')
+        );
+
+        $comments = $this->sendCommandToBusHelper($command);
+
+        if ($this->isCommandError($comments)) {
+            throw new Exception($comments->getMessage());
+        }
+
+        return view('partials.comment', ['comments' => $comments]);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return array
+     */
+    protected function getValidationRules(): array
+    {
+        return [
+            Comment::CONTENT => 'bail|required'
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     *
+     * @return array
+     */
+    protected function getValidationMessages(): array
+    {
+        return [
+            Comment::CONTENT => 'You type something to create a comment.'
+        ];
+    }
+}
