41db333cd39f27d6c3357d986f1b947e446e89d9
Gareth Lawson <garethpeter@gmail.com>
2017-04-24 11:16:20 +0200

    Make the logout button functional.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -51,13 +51,20 @@
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a href="{{ url('/logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     <div class="nav-btn">
                         <h4 class="nav-btn-header"><i class="fa fa-sign-out fa-lg nav-indent" aria-hidden="true"></i>
                         </h4>
                         <p class="nav-btn-text">Logout</p>
                     </div>
                 </a>
+                <form id="logout-form"
+                      action="{{ url('/logout') }}"
+                      method="POST"
+                      style="display: none;">
+                    {{ csrf_field() }}
+                </form>
             </li>
         </ul>
     </div>
