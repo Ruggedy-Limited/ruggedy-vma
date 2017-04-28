d82a436a24ebd0b5f4335375966f91c9cb6c91e0
Gareth Lawson <garethpeter@gmail.com>
2017-04-28 07:11:46 +0200

    Wrap menu-toggle code in a closure.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,9 @@
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
+(function ($) {
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+})(jQuery);
 
 (function ($) {
     $.ajaxSetup({
