e8c2688cc1217ec193186d10be05ddb066f9aef8
Gareth Lawson <garethpeter@gmail.com>
2017-05-04 17:04:06 +0200

    Add an admin flag to the settings index view.

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -1,6 +1,9 @@
 <div class="col-md-4 col-sm-6">
     <a href="{{ route('settings.user.edit', ['userId' => $user->getId()]) }}">
         <div class="content-card animated pulse-hover">
+            @if ($user->isAdmin())
+                <span class="label label-success pull-right t-s-10">admin</span>
+            @endif
             <h4>{{ $user->getName() }}</h4>
             <p>{{ $user->getEmail() }}</p>
         </div>
