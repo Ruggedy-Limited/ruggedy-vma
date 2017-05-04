f9ca9039d0035e7ef88d4d88c7cf49a34b62372e
Gareth Lawson <garethpeter@gmail.com>
2017-05-04 15:10:32 +0200

    Minor change to the message when there are no assets to display.

--- resources/views/partials/assets.blade.php
+++ resources/views/partials/assets.blade.php
@@ -12,7 +12,7 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <p class="p-l-8">No Assets here. The file is probably still in the queue for processing.</p>
+                <p class="p-l-8">No Assets here yet. The file is probably still in the queue for processing.</p>
                 <p class="p-l-8">If nothing appears within the next 20-30 minutes, something is probably wrong and you should
                     contact technical support.</p>
             </div>
