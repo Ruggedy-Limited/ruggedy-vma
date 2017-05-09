b42f5dfd0587f79e2d8045ff30c59729c2bd5196
Gareth Lawson <garethpeter@gmail.com>
2017-05-09 21:31:21 +0200

    Add a flag for adding a Google Tag Manager (GTM) container

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="_token" content="{{ csrf_token() }}">
-    <title>Ruggedy-App</title>
+    @include('partials.gtm-head')
+
+    <title>Ruggedy VMA</title>
+
     <link rel="stylesheet" href="{{ asset('/vendor/bootstrap/dist/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('/vendor/font-awesome/css/font-awesome.css') }}">
     <link rel="stylesheet" href="{{ asset("/css/animate.min.css") }}">
@@ -16,6 +19,7 @@
 </head>
 
 <body>
+@include('partials.gtm-body')
 <div id="wrapper">
     <div id="sidebar-wrapper">
         <ul class="sidebar-nav">
