2cecc8641c5732a16312039e56f2f8a95a75cf70
Gareth Lawson <garethpeter@gmail.com>
2017-05-18 19:06:36 +0200

    Add a 10 second wait for containers to initialise and make the docker exec command open a new tty.

--- docker-files/build.sh
+++ docker-files/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cp docker-files/.env ./
 docker-compose up -d
-docker exec ruggedy-vma /usr/share/nginx/html/ruggedy-vma/docker-files/post-docker-compose-up.sh
+echo "Waiting for containers to initialise..."
+sleep 10
+docker exec -t ruggedy-vma /usr/share/nginx/html/ruggedy-vma/docker-files/post-docker-compose-up.sh
