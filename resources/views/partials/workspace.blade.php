3b21f6eed07c9e03f417837f35c0df0c5bf0a6c3
Gareth Lawson <garethpeter@gmail.com>
2017-10-24 17:53:27 +0200

    Add convenience buttons to all the relevant partials/views.

--- resources/views/partials/workspace.blade.php
+++ resources/views/partials/workspace.blade.php
@@ -1,4 +1,19 @@
 <div class="col-md-4 col-sm-6">
+    @can (App\Policies\ComponentPolicy::ACTION_EDIT, $workspace)
+        <div class="convenience-buttons">
+            <a href="{{ route('workspace.edit', [$workspace->getRouteParameterName() => $workspace->getId()]) }}">
+                <button type="button" class="btn round-btn pull-right c-purple">
+                    <i class="fa fa-pencil fa-lg" aria-hidden="true"></i>
+                </button>
+            </a>
+            <a href="{{ route('workspace.delete', [$workspace->getRouteParameterName() => $workspace->getId()]) }}"
+               class="delete-link">
+                <button type="button" class="btn round-btn pull-right c-red">
+                    <i class="fa fa-trash-o fa-lg" aria-hidden="true"></i>
+                </button>
+            </a>
+        </div>
+    @endcan
     <a href="{{ route('workspace.view', ['workspaceId' => $workspace->getId()]) }}">
         <div class="card hovercard animated pulse-hover">
             <div class="cardheader c-white">
