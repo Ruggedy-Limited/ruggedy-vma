662cc24eeb35349af12d48b9f4c4707079712940
Gareth Lawson <garethpeter@gmail.com>
2017-10-24 18:04:04 +0200

    Updated version number

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
 
         @yield('content')
     </div>
-    <div class="version-info">Version: 0.1.8-beta</div>
+    <div class="version-info">Version: 0.1.9-beta</div>
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
